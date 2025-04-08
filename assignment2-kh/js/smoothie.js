74d86919765315cc79050d3c492b9983efdd8953
Kristina Hocken <200213022@student.georgianc.on.ca>
2025-04-08 17:45:13 -0400

    started js file, made the class and constructor, and mix it

---
+++ assignment2-kh/js/smoothie.js
@@ -0,0 +1,24 @@
+const output = document.querySelector('#output');
+/* set the const output to select the output id set in the <p> on the html*/
+
+/* Create a class 'Smoothie */
+class Smoothie {
+    // the properties of the smoothie
+    size;
+    base;
+    ingredients;
+    // constructor for the class
+    constructor(size, base, ingredients) {
+        this.size = size;
+        this.base = base;
+        this.ingredients = ingredients;
+    }
+    // method to make the smoothie
+    mixIt() {
+        // make an img element
+        const beverage = document.createElement("img");
+        // put the image path
+        let beverageImage = "";
+        
+    }
+};
