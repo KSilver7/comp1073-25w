d40dbe5b34bfa5622822cbec26d749b8614dc803
Kristina Hocken <200213022@student.georgianc.on.ca>
2025-04-08 22:10:36 -0400

    finished if else statements for the choice of image based on base type

--- assignment2-kh/js/smoothie.js
+++ assignment2-kh/js/smoothie.js
@@ -17,7 +17,7 @@ class Smoothie {
     mixIt() {
         // make an img element
         const beverage = document.createElement("img");
-        // put the image path
+        // put the image path. Original image from https://creazilla.com/media/clipart/842650/smoothie with a cc license.
         let beverageImage = "images/smoothie-purp.svg";
         // Figure out what base is used
         let smoothieBase;
@@ -26,6 +26,18 @@ class Smoothie {
             // grab the water based smoothie image instead
             beverageImage = "images/smoothie-blue.svg";
         }
-
+        if (this.base === "lemon-aid" || this.base === "tropical-punch" || this.base === "tomato-juice") {
+            // get the juice based smoothie image instead
+            beverageImage = "images/smoothie-pink.svg";
+        }
+        if (this.base === "yogurt" || this.base === "milk" || this.base === "lactose-free-milk" 
+            || this.base === "almond-milk" || this.base === "soymilk") {
+                // grab the dairy based smoothie image instead
+                beverageImage = "images/smoothie-yellow.svg";
+        }
+        else {
+            //grab the default smoothie image instead'
+            beverageImage = "images/smoothie-purp.svg";
+        }
     }
 };
