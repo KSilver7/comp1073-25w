59ddbe30d80bfd81af3b0f6156d8bca741e95021
Kristina Hocken <200213022@student.georgianc.on.ca>
2025-04-12 16:41:35 -0400

    setting up what i'll answer about charAt()

--- extra-lab/js/extralab.js
+++ extra-lab/js/extralab.js
@@ -1,1 +1,18 @@
 /* charAt() */
+// This is the reference to the HTML <p> where we want the output to be shown
+const output = document.querySelector('#output');
+
+/* Today we'll answer the following questions:
+
+What is the charAt() method?
+
+What does the charAt() method do?
+
+When would the charAt() method be used?
+
+Where would the charAt() method be used?
+
+How would the charAt() method be used?
+*/
+
+
