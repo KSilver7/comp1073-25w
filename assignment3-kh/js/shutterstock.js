e9434c4544288508af3863cd8f958ff9947731a9
Kristina Hocken <200213022@student.georgianc.on.ca>
2025-04-18 02:19:31 -0400

    added a while and if else  statement with an error message

--- assignment3-kh/js/shutterstock.js
+++ assignment3-kh/js/shutterstock.js
@@ -16,7 +16,7 @@ function fetchResults(event) {
     // prevent default to stop the form submitting
     event.preventDefault();
     // assemble the full URL
-    url = `${baseURL}?api_key=${key}&q=${searchTerm.value}`;
+    url = `${baseURL}?apikey=${key}&query=${searchTerm.value}`;
     // shutterstock requires that we add the headers to the request
     const headers = {
         "User-Agent": "COMP1073/1.0"
@@ -31,3 +31,40 @@ function fetchResults(event) {
     });
 };
 
+function displayResults(images) {
+    // clear out the old results here
+    while (section.firstChild) {
+        section.removeChild(section.firstChild);
+    };
+    let articles = json.response.docs;
+    // if there isn't an image, give an error msg as a little paragraph
+    if (!images) {
+        const paragraph = document.createElement('p');
+        paragraph.textContent = 'No images were found.';
+        section.appendChild(paragraph);
+        // else if image is found, do a for each loop and attach the image to the section
+    } else if (images.length > 0) {
+        images.forEach(image => {
+            // container for the img
+            const article = document.createElement('article');
+            // image itself
+            const img = document.createElement('img');
+            // description for the image
+            const caption = document.createElement('p');
+
+            img.src = image.assets.preview.url;
+            img.alt = image.description;
+            caption.textContent = image.description;
+            article.appendChild(img);
+            article.appendChild(caption);
+            // put the article in the page
+            section.appendChild(article);
+
+        });
+    } else {
+        // just in case there's no image, add an error message
+        const paragraph = document.createElement('p');
+        paragraph.textContent = 'No images were found.';
+        section.appendChild(paragraph);
+    };
+};
